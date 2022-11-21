a0a5dd26d06b321ea7e8132cdfb6604fdc8c48e5
Alex Anderson <andersonaj116@gmail.com>
2022-11-21 09:32:16 -0600

    Initial Commit

---
+++ Pace Calculator/Pace_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pace_CalculatorApp.swift
+//  Pace Calculator
+//
+//  Created by Alex Anderson on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Pace_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
